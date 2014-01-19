0c2b9b2ec38398b983cb3147d80b5f6bc3d8a71f
Dustin Spicuzza <dustin@virtualroadside.com>
2014-01-19 11:43:26 -0500

    Fix up the gyro test

--- subsystem_tests/src/robot.py
+++ subsystem_tests/src/robot.py
@@ -14,31 +14,35 @@ class MyRobot(wpilib.SimpleRobot):
         
         #self.digitalInput=wpilib.DigitalInput(4)
         self.gyro = wpilib.Gyro(1)
-        self.Joystick=wpilib.Joystick(1)
-        self.Joystick2=wpilib.Joystick(2)
-        self.Timer=wpilib.Timer()
-        self.jaguar=wpilib.Jaguar(1)
-        self.jaguar2=wpilib.CANJaguar(2)
-        self.jaguar3=wpilib.CANJaguar(3)
-        self.jaguar4=wpilib.CANJaguar(4)
-        self.drive = wpilib.RobotDrive(self.jaguar, self.jaguar2, self.jaguar3, self.jaguar4)#self.jaguar4=wpilib.Jaguar(4)
-        self.drive.SetSafetyEnabled(False)
+        
+        self.joystick=wpilib.Joystick(1)
+        self.joystick2=wpilib.Joystick(2)
+        
+        #self.jaguar=wpilib.Jaguar(1)
+        #self.jaguar2=wpilib.Jaguar(2)
+        #self.jaguar3=wpilib.Jaguar(3)
+        #self.jaguar4=wpilib.Jaguar(4)
+        #self.drive = wpilib.RobotDrive(self.jaguar, self.jaguar2, self.jaguar3, self.jaguar4)#self.jaguar4=wpilib.Jaguar(4)
+        #self.drive.SetSafetyEnabled(False)
         
 
     def OperatorControl(self):
-        print(self.IsEnabled())
+        
+        print("MyRobot::OperatorControl()")
+        
+        wpilib.GetWatchdog().SetEnabled(False)
         
        # dog = wpilib.GetWatchdog()
         #dog.setEnabled(True)
         #dog.SetExpiration(10)
         
-        while self.IsOperatorControl():  
+        while self.IsOperatorControl() and self.IsEnabled():  
           #  dog.Feed()
             #self.drive.MecanumDrive_Cartesian(self.Joystick.GetY(), self.Joystick.GetX(), self.Joystick2.GetX(), 0)
             
-            self.gyro.GetAngle()
-            wpilib.Wait(0.01)
             wpilib.SmartDashboard.PutNumber('GyroAngle', self.gyro.GetAngle())
+            
+            wpilib.Wait(0.01)
             
             
         
