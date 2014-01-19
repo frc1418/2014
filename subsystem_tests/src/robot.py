d97864713d73303a6098fa01199467b7d60e6f06
Matt <Matt@Matt-PC2.gm.fccps>
2014-01-19 15:06:16 -0500

    The Robot subSystemtest has an error which i sent to dustin

--- subsystem_tests/src/robot.py
+++ subsystem_tests/src/robot.py
@@ -1,3 +1,4 @@
+from pyfrc.wpilib._wpilib._pid_controller import PIDController
 
 try:
     import wpilib
@@ -14,11 +15,15 @@ class MyRobot(wpilib.SimpleRobot):
         
         #self.digitalInput=wpilib.DigitalInput(4)
         self.gyro = wpilib.Gyro(1)
-        
         self.joystick=wpilib.Joystick(1)
         self.joystick2=wpilib.Joystick(2)
+        self.jaguar=wpilib.Jaguar(1)
+        self.accelerometer=wpilib.ADXL345_I2C(1, wpilib.ADXL345_I2C.kRange_2G)
+        self.p=1
+        self.i=0
+        self.d=0
+        self.pid = wpilib.PIDController(p, i, d, self.gyro, self.jaguar)
         
-        #self.jaguar=wpilib.Jaguar(1)
         #self.jaguar2=wpilib.Jaguar(2)
         #self.jaguar3=wpilib.Jaguar(3)
         #self.jaguar4=wpilib.Jaguar(4)
@@ -27,25 +32,40 @@ class MyRobot(wpilib.SimpleRobot):
         
 
     def OperatorControl(self):
-        
+        self.pid.Enable()
         print("MyRobot::OperatorControl()")
         
         wpilib.GetWatchdog().SetEnabled(False)
         
-       # dog = wpilib.GetWatchdog()
+        #dog = wpilib.GetWatchdog()
         #dog.setEnabled(True)
         #dog.SetExpiration(10)
         
         while self.IsOperatorControl() and self.IsEnabled():  
-          #  dog.Feed()
+            #dog.Feed()
             #self.drive.MecanumDrive_Cartesian(self.Joystick.GetY(), self.Joystick.GetX(), self.Joystick2.GetX(), 0)
             
             wpilib.SmartDashboard.PutNumber('GyroAngle', self.gyro.GetAngle())
+            axis=self.accelerometer.GetAccelerations()
+            wpilib.SmartDashboard.PutNumber('Acceleration Axis X', axis.XAxis)
+            wpilib.SmartDashboard.PutNumber('Acceleration Axis Y', axis.YAxis)
+            wpilib.SmartDashboard.PutNumber('Acceleration Axis Z', axis.ZAxis)
+            
+            wpilib.SmartDashboard.GetNumber('')
+            self.PIDMove()
             
             wpilib.Wait(0.01)
             
             
+    def PIDMove(self):
+        self.pid.SetSetpoint(10)
         
+        
+        
+        
+        
+        
+            
 def run():
     
     robot = MyRobot()
