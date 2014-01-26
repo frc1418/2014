755e374416feb9741cbcb1a58e44283630822897
Dustin Spicuzza <dustin@virtualroadside.com>
2014-01-25 22:20:30 -0500

    Make the driving subsystem more complete

--- robot/robot/src/components/drive.py
+++ robot/robot/src/components/drive.py
@@ -1,20 +1,42 @@
-'''	
-	Driving component
-'''
 
 class Drive(object):
+	'''
+		The sole interaction between the robot and its driving system
+		occurs here. Anything that wants to drive the robot must go
+		through this class.
+	'''
 
 	def __init__(self, robotDrive):
+		'''
+			Constructor. 
+			
+			:param robotDrive: a `wpilib.RobotDrive` object
+		'''
+		
+		# set defaults here
+		self.magnitude = 0
+		self.direction = 0
+		self.rotation = 0
+		
 		self.robotDrive = robotDrive
+		
 
 	#
 	# Verb functions
 	#
 
-	def move(self, x, y, z):
-		self.x = x
-		self.y = y
-		self.z = z
+	def move(self, magnitude, direction, rotation):
+		'''
+			Causes the robot to move
+		
+			:param magnitude: The speed that the robot should drive in a given direction. [-1.0..1.0] 
+			:param direction: The direction the robot should drive in degrees. The direction and magnitude are independent of the rotation rate. 
+			:param rotation:  The rate of rotation for the robot that is completely independent of the magnitude or direction. [-1.0..1.0]
+		'''
+		
+		self.magnitude = magnitude
+		self.direction = direction
+		self.rotation = rotation
 
 
 	#
@@ -23,9 +45,9 @@ class Drive(object):
 
 	def doit(self):
 
-		self.robotDrive.MecanumDrive_Cartesian(self.x, self.y, self.z)
+		self.robotDrive.MecanumDrive_Polar(self.magnitude, self.direction, self.rotation)
 
-		# reset things to defaults
-		self.x = 0
-		self.y = 0
-		self.z = 0
+		# by default, the robot shouldn't move
+		self.magnitude = 0
+		self.direction = 0
+		self.rotation = 0
