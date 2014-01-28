253cda917bad93023c0efb46491bf6c122dd1c5e
Twinters007 <twinters008@gmail.com>
2014-01-27 22:41:15 -0500

    Added comment

--- robot/robot/src/components/catapult.py
+++ robot/robot/src/components/catapult.py
@@ -1,7 +1,7 @@
-class Catapult (object):
+    p0class Catapult (object):
     #This is matt's catapult code. don't make fun of it.
     def __init__ (winch, solenoid):
-        #im assuming that the potentiometer max is 1 and the potentiometer min is 0
+        #im assuming that the potentiometer max is 1 and the potentiometer min is 0 --- Matt, the potentiometer is whatever we set it to, so you should talk to Shayne about how to do that
         angle=potentiometer.Get()
         self.winch=winch
         self.solenoid=solenoid
