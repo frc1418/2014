d41b8d04cb00be0be0550ccc327480c15da9d2ba
Twinters007 <twinters008@gmail.com>
2014-01-29 21:05:22 -0500

    Added 5th and 6th Jags. May need to switch around the CAN jagaur.

--- robot/electrical_test/src/robot.py
+++ robot/electrical_test/src/robot.py
@@ -9,21 +9,19 @@ class MyRobot(wpilib.SimpleRobot):
     state = 1
     def __init__ (self):
         super().__init__()
-<<<<<<< HEAD
    
-        print("Tim Winters")
         
-=======
-        print("Tim Winters II")   
->>>>>>> 932d5c04c69b854047f3ac275a4cc161f31c31ff
+        print("Tim Winters II")
         #self.digitalInput=wpilib.DigitalInput(4)
         self.Joystick=wpilib.Joystick(1)
         self.Joystick2=wpilib.Joystick(2)
         self.Timer=wpilib.Timer()
-        self.jaguar=wpilib.Jaguar(1)
+        self.jaguar1=wpilib.Jaguar(1)
         self.jaguar2=wpilib.Jaguar(2)
         self.jaguar3=wpilib.Jaguar(3)
         self.jaguar4=wpilib.Jaguar(4)
+        self.jaguar5=wpilib.CANJaguar(5)
+        self.jaguar6=wpilib.Jaguar(6)
         self.drive = wpilib.RobotDrive(self.jaguar, self.jaguar2, self.jaguar3, self.jaguar4)#self.jaguar4=wpilib.Jaguar(4)
     
         self.drive.SetSafetyEnabled(False)
@@ -39,32 +37,14 @@ class MyRobot(wpilib.SimpleRobot):
         while self.IsOperatorControl()and self.IsEnabled():
           #  dog.Feed()
             self.drive.MecanumDrive_Cartesian(self.Joystick.GetY(), self.Joystick.GetX(), self.Joystick2.GetX(), 0)
+            x = self.Joystick.GetButton()
+            if x==3:
+                self.jaguar6.Set(-1)
+            if x==5:
+                self.jaguar6.Set(1)    
             wpilib.Wait(0.01)
             
-    def move_motor(self):
-       # print(self.state, self.Timer.Get(), self.digitalInput.Get())
-        '''if self.state is 1:
-            self.Timer.Reset()
-            self.jaguar.Set(self.Joystick.GetY())
-            if self.digitalInput.Get()==True:
-                self.Timer.Reset()
-                self.state =  2
-            
-        elif self.state is 2:  
-            self.jaguar.Set(self.Joystick.GetY())
-            if self.Timer.HasPeriodPassed(1):
-                self.state = 3
-            if not self.digitalInput.Get():
-                self.state=1
-        elif self.state is 3:
-            self.jaguar.Set(1)
-            if self.Timer.HasPeriodPassed(3):
-                self.state = 4
-        elif self.state is 4:
-             self.jaguar.Set(-1)
-             if(self.Timer.HasPeriodPassed(2)):
-                 self.state=1   
-        '''
+'''
         
 def run():
     
