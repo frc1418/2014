15893161ef1d3a6109156b804519e23a0c8e5781
Ben1418 <ben.m.rice@gmail.com>
2014-02-01 11:45:33 -0500

    finished first for loop

--- driver_station/src/targeting/detect.py
+++ driver_station/src/targeting/detect.py
@@ -29,52 +29,69 @@ def process_image(img):
     cv2.waitKey(0)
     # threshold hsv
     hsv = cv2.cvtColor(img, cv2.cv.CV_BGR2HSV)
-    h, s, v = cv2.split(img)
+    h, s, v = cv2.split(hsv)
 
     # these parameters will find 'green' on the image
     h = threshold_range(h, 0, 255)
     s = threshold_range(s, 150, 255)
-    v = threshold_range(v, -1, 256)
-
+    v = threshold_range(v, 100, 170)
+    cv2.imshow("v",v)
     #combine them
     combined = cv2.bitwise_and(h, cv2.bitwise_and(s, v))
     cv2.imshow('combined', combined)
+    cv2.waitKey(0)
     # fill in the holes
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2), anchor=(1,1))
-    morphed_img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=9)
-
+    morphed_img = cv2.morphologyEx(combined, cv2.MORPH_CLOSE, kernel, iterations=9)
     # analyze particles
-    color_img = cv2.cvtColor(img, cv2.cv.CV_GRAY2BGR)
-    cv2.imshow("color", morphed_img)
+    cv2.cvtColor(morphed_img, cv2.cv.CV_GRAY2BGR)
+    cv2.imshow("morphed", morphed_img)
     cv2.waitKey(0)
-    contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)  
-    p = []   
+    contours, hierarchy = cv2.findContours(morphed_img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)  
+    p = []  
+    vertical_targets = []
+    horizontal_targets=[] 
     # remove small particles
     # for each particle
+    
+    
+    #    contour = contours[i]
+    
     for contour in contours:
-            #p = cv2.approxPolyDP(contour, 45, False)
+        #p = cv2.approxPolyDP(contour, 45, False)
+    # filtering smaller contours from pictures
+        if  cv2.contourArea(contour) >=  40:
+            p.append(contour)
         
-            if  cv2.contourArea(contour) >=  40:
-                p.append(contour)
+        elif   cv2.contourArea(contour) < 40:
+            continue
+        #getting lenghts of sides of rectangle around the contours
+        x, y, w, h = cv2.boundingRect(contour)
+        
+        if (w > h):
+            horizontal_targets.append(contour)
+        elif (h > w):
+            vertical_targets.append(contour)
             
-            elif   cv2.contourArea(contour) < 40:
-                continue
-    contours = cv2.drawContours(color_img, p, -1, (0,0,255), thickness=2)
-    cv2.imshow('contours', contours)
-
-        # score rectangularity
-    x, y, w, h = cv2.boundingRect(contours)
+    conto = morphed_img
+    cv2.drawContours(conto, horizontal_targets, -1, (44,0,232), thickness=2) 
+    cv2.imshow('h identify', conto)    
+    cv2.waitKey(0)
+    
+    ''' 
+    cv2.imshow('contours', conto)
+    cv2.waitKey(0)
+    
+    # score rectangularity
+    
     ((centerX, centerY), (rw, rh), rotation) = cv2.minAreaRect(p)  
     # sometimes minAreaRect decides to rotate the rectangle too much.
-            # detect that and fix it. 
-       
+            # detect that and fix it.       
     if (w > h and rh < rw) or (h > w and rw < rh):
         rh, rw = rw, rh  # swap
-        rotation = -90.0 - rotation
+        rotation = -90.0 - rotation'''
         # score aspect ratio vertical
-    vertical_targets = []
-    horizontal_targets = []
-    
+        
         # score aspect ratio horizontal
         
         # determine if horizontal
@@ -83,10 +100,11 @@ def process_image(img):
         
     # 
     # Match up the targets to each other
-    #
+    
         
     # for each vertical target
     
+        
         # for each horizontal target
         
             # measure equivalent rectangle sides
@@ -119,6 +137,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "Usage: %s image" % (sys.argv[0])
         exit(1)
+        
     
     img = cv2.imread(sys.argv[1])
     process_image(img)
