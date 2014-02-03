f59dda39c17fdc2cb4e4e551d3ff41f71b0b084b
killerhamster222 <matt.puentes@gmail.com>
2014-02-03 17:19:12 -0500

    We fixed tikmmy's bullshit driving and some other errors in eletrical test.

--- robot/electrical_test/src/robot.py
+++ robot/electrical_test/src/robot.py
@@ -25,21 +25,20 @@ class MyRobot(wpilib.SimpleRobot):
         self.winch=wpilib.CANJaguar(5)
         self.intake=wpilib.Jaguar(6)
         self.compressor=wpilib.Compressor(1,1)
-        print (self.compressor.GetPressureSwitchValue())
-        self.compressor.Start()
-        print (self.compressor.Enabled())
+        #self.compressorRelayHacks = wpilib.Relay(1);
+        #self.relayTest = wpilib.Relay(2);
         self.solenoid_intake_up=wpilib.Solenoid(1)
         self.solenoid_intake_down=wpilib.Solenoid(2)
         self.solenoid_release_release=wpilib.Solenoid(3)
         self.solenoid_release_engage=wpilib.Solenoid(4)
         self.solenoid_kicker_out=wpilib.Solenoid(5)
         self.solenoid_kicker_in=wpilib.Solenoid(6)
-        self.drive = wpilib.RobotDrive(self.LF_motor, self.LR_motor, self.RF_motor, self.RR_motor)#self.jaguar4=wpilib.Jaguar(4)
+        self.drive = wpilib.RobotDrive(self.RF_motor, self.LR_motor, self.LF_motor, self.RR_motor)#self.jaguar4=wpilib.Jaguar(4)
+        self.drive.SetSafetyEnabled(False)
         self.gyro = wpilib.Gyro(1)
         self.ultrasonic=wpilib.AnalogChannel(2)
         self.potentiometer=wpilib.AnalogChannel(3)
         self.accelerometer=wpilib.ADXL345_I2C(1, wpilib.ADXL345_I2C.kRange_2G)
-        self.drive.SetSafetyEnabled(False)
     
    
     def OperatorControl(self):
@@ -51,14 +50,14 @@ class MyRobot(wpilib.SimpleRobot):
         while self.IsOperatorControl()and self.IsEnabled():
             #  dog.Feed()
             #Driving
-            self.drive.MecanumDrive_Cartesian(self.joystick.GetY(), -1*self.joystick.GetX(), -1*self.joystick2.GetX(), 0)
+            self.drive.MecanumDrive_Cartesian(self.joystick.GetY(), 1*self.joystick2.GetX(), 1*self.joystick.GetX(), 0)
             self.Intake()
             self.Catapult()
             self.Solenoids()
             self.SmartDash()
             self.Air()
-            print (int (self.joystick.GetRawButton(8)))
-            wpilib.Wait(0.01)
+            #print (int (self.joystick.GetRawButton(8)))
+            wpilib.Wait(0.05)
    
             
     def Intake(self):
@@ -74,7 +73,6 @@ class MyRobot(wpilib.SimpleRobot):
             
     def Catapult(self):
         self.winch.Set(self.joystick.GetZ())
-        self.compressor.Start()
         
     def Solenoids(self):
         self.solenoid_intake_up.Set(self.joystick2.GetRawButton(1))
@@ -95,14 +93,17 @@ class MyRobot(wpilib.SimpleRobot):
         wpilib.SmartDashboard.PutNumber('Acceleration Axis Z', axis.ZAxis)
         
     def Air(self):
-        self.compressor.Start()
         x = self.joystick.GetRawButton(8)
         y = self.joystick.GetRawButton(9)
         if x:
-            self.compressor.SetRelayValue(1)
+            self.compressor.Start()
+            #self.compressorRelayHacks.Set(wpilib.Relay.kOn)
+            #self.relayTest.Set(wpilib.Relay.kForward)
         elif y:
-            self.compressor.SetRelayValue(0)
             self.compressor.Stop()
+            #self.compressorRelayHacks.Set(wpilib.Relay.kOff)
+            #self.relayTest.Set(wpilib.Relay.kOff)
+        print(self.compressor.Enabled())
             
 def run():
     robot = MyRobot()
