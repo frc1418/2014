acc899c32f8806ccaf4c714c43132c66bf50d59d
Infinxyxs <Infinxyxs@gmail.com>
2014-02-05 17:07:37 -0500

    Edits to catapult, now includes double solenoid, solenoid lock is taken out

--- robot/robot/src/components/catapult.py
+++ robot/robot/src/components/catapult.py
@@ -1,16 +1,17 @@
 class Catapult (object):
     #This is matt's catapult code. don't make fun of it.
-    def __init__ (self, winch, solenoid, potentiometer, analog_channel, timer):
+    def __init__ (self, winch, activatesolenoid, potentiometer, analog_channel, timer):
         #im assuming that the potentiometer max is 1 and the potentiometer min is 0 --- Matt, the potentiometer is whatever we set it to, so you should talk to Shayne about how to do that
         self.opticalsensor = analog_channel
 
         self.potentiometer = potentiometer 
         self.winch=winch
-        self.solenoid=solenoid
+        self.activatesolenoid=activatesolenoid
         self.timer = timer
         
         self.tempwinch=0
-        self.tempsolenoid=False
+        self.tempsolenoid1=False
+        self.tempsolenoid2=False
         self.ballready = False
         self.solenoidlock = False
         #i am assuming launchangle will be defined by the smart-dashboard-ish thing dusitin wants to make, for now it is 0
@@ -24,7 +25,7 @@ class Catapult (object):
             self.solenoidlock = False 
             #self.timer.Reset()
             #self.timer.Stop()
-        
+
         self.launcherup=True
         if Potentiometer > 0 and self.solenoidlock is False:
             self.tempwinch=1
@@ -32,18 +33,24 @@ class Catapult (object):
             self.tempwinch=0
             self.launcherup=False
             #Matt, this section doesn't make sense. -S & L
+    def prepare(self):
+        if self.launcherup == False and self.solenoidlock == False and self.ballready==True:
+            self.tempsolenoid2=True
+            self.launcherup=False
+            self.solenoidlock=True
 
     def launch(self):
-        if self.solenoidlock == False: #and self.ballready == True:
+        if self.solenoidlock == False and self.ballready == True:
             '''remember to take off the pound on ballready'''
-            self.tempsolenoid=True
+            self.tempsolenoid2=False
+            self.tempsolenoid1=True
             self.launcherup=True
             self.solenoidlock = True
             #self.timer.Reset()
             self.timer.Start()
             print("Has Launched")
         else:
-            self.tempsolenoid=False
+            self.tempsolenoid1=False
     
     def check_ready(self, analog_channel):
         if analog_channel >= 1:
@@ -55,8 +62,10 @@ class Catapult (object):
         return self.launcherup
     def doit(self):
         #could be any port?
-        print(self.tempsolenoid)
+        print(self.tempsolenoid1,self.tempsolenoid2)
         self.winch.Set(self.tempwinch)
-        self.solenoid.Set(self.tempsolenoid)
+        self.activatesolenoid.Set(self.tempsolenoid1,self.tempsolenoid2)
  
+
+
 
