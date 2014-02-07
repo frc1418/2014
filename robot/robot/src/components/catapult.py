9343064fe97570be88ce4c52f2e67a43e6268075
Leon Tan <lleontan@gmail.com>
2014-02-07 16:09:06 -0500

    Timer added to catapult. driving still not working. driving is copy pasted from electrcal test.

--- robot/robot/src/components/catapult.py
+++ robot/robot/src/components/catapult.py
@@ -8,6 +8,7 @@ class Catapult (object):
     def __init__ (self, winch, activatesolenoid, potentiometer, analog_channel, timer):
         #im assuming that the potentiometer max is 1 and the potentiometer min is 0 --- Matt, the potentiometer is whatever we set it to, so you should talk to Shayne about how to do that
         self.opticalsensor = analog_channel
+        self.shootTimer=timer
 
         self.potentiometer = potentiometer 
         self.winch=winch
@@ -18,32 +19,27 @@ class Catapult (object):
         self.tempsolenoid1=False
         self.tempsolenoid2=False
         self.ballready = False
-        self.solenoidlock = False
         #i am assuming launchangle will be defined by the smart-dashboard-ish thing dusitin wants to make, for now it is 0
         self.launchangle=0
         
         self.launcherup=True
         
     def pulldown(self, Potentiometer):
-        if self.timer.HasPeriodPassed(1) == True:
-            self.solenoidlock = False 
-            #self.timer.Reset()
-            #self.timer.Stop()
 
         self.launcherup=True
-        if Potentiometer > 0 and self.solenoidlock is False:
+        if Potentiometer > 0:
             self.tempwinch=1
         elif self.winch.GetForwardLimitOK():
             self.tempwinch=0
             self.launcherup=False
             #Matt, this section doesn't make sense. -S & L
     def launch(self):
-        if self.solenoidlock == False and self.ballready == True:
-            '''remember to take off the pound on ballready'''
+        print("testing")
+        if  self.ballready == True:
+            print("Lauching")
             self.tempsolenoid2=False
             self.tempsolenoid1=True
             self.launcherup=True
-            self.solenoidlock = True
             #self.timer.Reset()
             self.timer.Start()
         else:
@@ -61,7 +57,13 @@ class Catapult (object):
         #could be any port?
         #print(self.tempsolenoid1,self.tempsolenoid2)
         self.winch.Set(self.tempwinch)
+        if self.shootTimer.HasPeriodPassed(.8):
+            self.activatesolenoid=True
+            self.shootTimer.Reset()
+            self.shootTimer.Stop()
         if self.tempsolenoid1 is True:
+            self.shootTimer.Start()
+            
             self.activatesolenoid.Set(wpilib.DoubleSolenoid.kForward)
         else:
             self.activatesolenoid.Set(wpilib.DoubleSolenoid.kOff)
--- robot/robot/src/robot.py
+++ robot/robot/src/robot.py
@@ -132,7 +132,7 @@ class MyRobot(wpilib.SimpleRobot):
                 self.catapult.pulldown(potentiometer1)
             
             #self.intake.wheels(intakedirection,launcherup)
-            
+            self.robot_drive.MecanumDrive_Cartesian(self.joystick1.GetY(), self.joystick1.GetX(), -1*self.joystick2.GetX())
             self.update()
             
             wpilib.Wait(self.control_loop_wait_time)
