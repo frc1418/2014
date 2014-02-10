8343f191fbb9c7b50d68b7c5c7718a004f8872c9
Leon Tan <lleontan@gmail.com>
2014-02-10 17:09:47 -0500

    hello

--- robot/robot/src/autonomous/main.py
+++ robot/robot/src/autonomous/main.py
@@ -96,6 +96,7 @@ class main(object):
         if time_elapsed < 0.5:
             # Get the arm down so that we can winch
             self.intake.armDown()
+            self.catapult.dogIn()
         elif time_elapsed < 1.5:
             # The arm is at least far enough down now that
             # the winch won't hit it, start winching
