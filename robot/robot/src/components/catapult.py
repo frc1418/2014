5bedac5ce7bbbead8aace47a726a4ea53e4c2b4d
Leon Tan <lleontan@gmail.com>
2014-02-10 16:00:48 -0500

    commented out dog on joystick button 2. Does the same thing as lauch but will never turn off.

--- robot/robot/src/components/catapult.py
+++ robot/robot/src/components/catapult.py
@@ -78,6 +78,7 @@ class Catapult (object):
             if self.winch.GetForwardLimitOK():
                 self.winch.Set(0)
                 
+                
         elif self.cState==LAUNCH:
             self.activateSolenoid.Set(wpilib.DoubleSolenoid.kForward)
             if not self.time:
--- robot/robot/src/robot.py
+++ robot/robot/src/robot.py
@@ -164,14 +164,15 @@ class MyRobot(wpilib.SimpleRobot):
             if self.joystick1.GetRawButton(1):
                 self.catapult.launchNoSensor()
             if self.joystick2.GetRawButton(2):
-                self.catapult.dogIn();
-            
+                #self.catapult.dogIn();
+                pass
             #
             # Other
             #
            
             self.sendToSmartDashboard()
             self.update()
+            
             
             preciseDelay.wait()
             
