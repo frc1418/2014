5e54666a1fde41d47ebda90910bad67f189ff956
killerhamster222 <matt.puentes@gmail.com>
2014-02-16 15:36:53 -0500

    I MADE THE GYRO THING FUCKING FINALLY

--- driver_station/src/ui/widgets/robot_angle_widget.py
+++ driver_station/src/ui/widgets/robot_angle_widget.py
@@ -1,24 +1,9 @@
-#
-#   This file is part of KwarqsDashboard.
-#
-#   KwarqsDashboard is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, version 3.
-#
-#   KwarqsDashboard is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details. 
-#
-#   You should have received a copy of the GNU General Public License
-#   along with KwarqsDashboard.  If not, see <http://www.gnu.org/licenses/>.
-#
-
-
 import math
 import os
 
 import gtk
+
+import cairo
 
 from .. import util
 
@@ -27,23 +12,21 @@ class RobotAngleWidget(gtk.DrawingArea):
         When the robot reports the angle of the platform, draw the
         platform on the UI at that particular angle. 
         
-        Additionally, show frisbees on the platform, and move them with
-        it. In practice, operators rarely use this widget. However, its
-        a great visual aid for demonstrations, or for debugging the 
-        robot. 
-    
             TODO: Make this more general, we hardcoded everything here
     '''
     
-    def __init__(self):
+    def __init__(self, NetworkTable):
+        self.netTable = NetworkTable
+        
         gtk.DrawingArea.__init__(self)
+        self.imageBG = util.surface_from_png('robotAngleWidgetBG.png')
+        self.imageFG = util.surface_from_png('robotAngleWidgetFG.png')
+        
 
         self.angle = 0
-        self.size = 150
+        self.set_size_request(150,150)
         
-        #self.background = util.pixbuf_from_file("robotAngleWidgetBG.png")
-        #self.indicator = util.pixbuf_from_file("robotAngleWidgetFG.png")
-    
+        self.connect('expose-event', self.on_expose)
     
     '''def connect(self, table, key):
         import network_tables
@@ -53,36 +36,31 @@ class RobotAngleWidget(gtk.DrawingArea):
         self.set_angle(value)
     
     def set_angle(self, angle):
-        angle = angle % 360
-        
         if angle != self.angle:
             self.angle = angle
             self.queue_draw()
         
     def on_expose(self, widget, event):
-        
-        # background
-        event.window.draw_pixbuf(None, self.background, 0, 0, 150, 150)
-        event.window.draw_pixbuf(None, self.indicator, 0, 0, 150, 150)
-        
         cxt = event.window.cairo_create()
         
-        # angle text
-        cxt.move_to(75, 75)
-        cxt.set_line_width(3)
-        cxt.set_source_rgb(0,0,0)
-        
-        # the math
+        cxt.set_source_surface(self.imageBG)
+        cxt.paint()
+        x=self.netTable.GetNumber('GyroAngle')
+        print (self.netTable.GetNumber('GyroAngle'), math.radians(self.netTable.GetNumber('GyroAngle')))
+        cxt.translate(75,75)
+        cxt.rotate(math.radians(x))
+        cxt.translate(-75,-75)
+
+        cxt.set_source_surface(self.imageFG)
+        '''# the math
         x = math.cos(self.angle)
         y = math.sin(self.angle)
         l = math.sqrt(x^2+y^2)
         
-        cxt.line_to(x*l+75,y*l+75)
-        
         cxt.set_font_size(20)
         cxt.show_text('%.2f' % self.angle)
-        
-        cxt.set_source_rgb(0,0,0)
-        cxt.fill_preserve()
         cxt.stroke()
-
+        '''
+        
+        
+        cxt.paint()
