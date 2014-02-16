6aad755aa606b2297cc8aa9d419d18f495c463f4
Dustin Spicuzza <dustin@virtualroadside.com>
2014-02-15 22:48:03 -0500

    Make the timed autonomous modes a bit simpler, and named better

--- robot/robot/src/autonomous/hot_shoot.py
+++ robot/robot/src/autonomous/hot_shoot.py
@@ -4,42 +4,29 @@ try:
 except ImportError:
     from pyfrc import wpilib
 
-class main(object):
-    '''autonomous program'''
-    
-    DEFAULT = False
-    MODE_NAME = "Hot shoot"
-    
-    def __init__ (self, components):
-        ''' initialize'''
-        super().__init__()
-        self.drive = components['drive']
-        self.intake = components['intake']
-        self.catapult = components['catapult']
-        
-        # number of seconds to drive forward, allow us to tune it via SmartDashboard
-        wpilib.SmartDashboard.PutNumber('AutoDriveTime', 1.4)
-        wpilib.SmartDashboard.PutNumber('AutoDriveSpeed', 0.5)
-        
-        wpilib.SmartDashboard.PutBoolean("IsHot", False)
+import timed_shoot
 
+class HotShootAutonomous(timed_shoot.TimedShootAutonomous):
+    '''
+        Based on the TimedShootAutonomous mode. Modified to allow
+        shooting based on whether the hot goal is enabled or not.
+    '''
+    
+    DEFAULT = True
+    MODE_NAME = "Hot shoot"
+
+    def __init__(self, components):
+        super().__init__(components)
+        
+        wpilib.SmartDashboard.PutBoolean('IsHot', False)
 
     def on_enable(self):
         '''these are called when autonomous starts'''
         
-        self.drive_time = wpilib.SmartDashboard.GetNumber('AutoDriveTime')
-        self.drive_speed = wpilib.SmartDashboard.GetNumber('AutoDriveSpeed')
+        super().on_enable()
         
         self.decided = False
-        self.started = False
-        
-        print("Team 1418 autonomous code for 2014")
-        print("-> Drive time:", self.drive_time, "seconds")
-        print("-> Drive speed:", self.drive_speed)
-        
-        #print("-> Battery voltage: %.02fv" % wpilib.DriverStation.GetInstance().GetBatteryVoltage())
-        
-        
+        self.start_time = None
     
     def on_disable(self):
          '''This function is called when autonomous mode is disabled'''
@@ -48,50 +35,52 @@ class main(object):
     def update(self, time_elapsed):   
         '''The actual autonomous program'''     
        
-       
+        # always keep the arm down
+        self.intake.armDown()
+        
+        # wait a split second for the arm to come down, then
+        # keep bringing the catapult down so we're ready to go
+        if time_elapsed > 0.3:
+            self.catapult.pulldown()
+        
+            
         # decide if it's hot or not
         if not self.decided:
             self.hot = wpilib.SmartDashboard.GetBoolean("IsHot")
             
             if self.hot:
                 self.decided = True
-                self.start_time = time_elapsed
                 
             elif time_elapsed > 6:
+                # at 6 seconds, give up and shoot anyways
                 self.decided = True
-                self.start_time = time_elapsed
        
         # always pulldown
         if time_elapsed > 0.3:
             self.catapult.pulldown()
             
        
-        if time_elapsed < 0.3:
-            # Get the arm down so that we can winch
-            self.intake.armDown()
-        
-        elif time_elapsed < 1.5:
-            # The arm is at least far enough down now that
-            # the winch won't hit it, start winching
-            self.intake.armDown()
+        # wait some period before we start driving
+        if time_elapsed < self.drive_wait:
+            pass
         
         else:
-            
-            if not self.started and self.decided:
-                self.decided = True
-                self.started = True
-                self.start_time = time_elapsed
-            
+                         
             if self.decided:
                 
+                # only set this once, so we can calculate time from this
+                # point on 
+                if self.start_time is None:
+                    self.start_time = time_elapsed
+                
+                
                 time_elapsed = time_elapsed - self.start_time
-            
                 
                 if time_elapsed < self.drive_time:
                     # Drive slowly forward for N seconds
                     self.drive.move(0, self.drive_speed, 0)
                     
                     
-                elif time_elapsed < 2.0 + self.drive_time + 1.0:
+                elif time_elapsed < self.drive_time + 1.0:
                     # Finally, fire and keep firing for 1 seconds
                     self.catapult.launchNoSensor()
