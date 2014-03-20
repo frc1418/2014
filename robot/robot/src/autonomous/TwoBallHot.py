299b0979a8129665a9c15464c12ecc740d824316
Leon Tan <lleontan@gmail.com>
2014-03-19 22:49:21 -0400

    TwoBall Stuff

--- robot/robot/src/autonomous/TwoBallHot.py
+++ robot/robot/src/autonomous/TwoBallHot.py
@@ -4,9 +4,10 @@ except ImportError:
     from pyfrc import wpilib
 from common.autonomous_helper import StatefulAutonomous, timed_state
 
+
 class TwoBall(StatefulAutonomous):
     
-    MODE_NAME = 'Two balls'
+    MODE_NAME = 'Two Balls Hot'
     DEFAULT = False
     
     def __init__(self, components):
@@ -54,48 +55,48 @@ class TwoBall(StatefulAutonomous):
     #
     #Please contact Timmy and get him to explain the timngs on his old TwoBall class
     
-    @timed_state(time=7.5, next_state='launch2')    
+    @timed_state(duration=7.5, next_state='launch2')    
     def launch2(self):
         
             # Finally, fire and keep firing for 1 seconds
             self.catapult.launchNoSensor()
             self.intake.ballIn()
     
-    @timed_state(time=6.5, next_state='next_ball2')        
+    @timed_state(duration=6.5, next_state='next_ball2')        
     def next_ball2(self):
                     
             self.drive.move(0, self.drive_speed, 0)
             self.intake.ballIn()
     
-    @timed_state(time=5.5, next_state='next_ball1')        
+    @timed_state(duration=5.5, next_state='next_ball1')        
     def next_ball1(self):
             self.drive.move(0, -1*self.drive_speed, 0)
             self.intake.ballIn()
     
-    @timed_state(time=1, next_state='drive_wait', first=True)
+    @timed_state(duration=1, next_state='drive_wait', first=True)
     def drive_wait(self, tm, state_tm):
         self.intake.armDown
     
-    @timed_state(time=2,next_state='drive_start')
+    @timed_state(duration=2,next_state='drive_start')
     def drive_start(self):
          self.drive.move(0, self.drive_speed, 0)
          self.intake.armDown()
     
-    @timed_state(time=4,next_state='try_shoot')
+    @timed_state(duration=4,next_state='try_shoot')
     def try_shoot(self,tm,state_tm):
         launch(tm)
     
     def pre_drive(self, tm):
         pass
     
-    @timed_state(time=1.4, next_state='launch')
+    @timed_state(duration=1.4, next_state='launch')
     def drive(self, tm, state_tm):
         self.drive.move(0, self.drive_speed, 0)
-    @timed_state(time=1,next_state='drive')
+    @timed_state(duration=1,next_state='drive')
     def rotate_move(self,tm,state_tm):
         pass
     
-    @timed_state(time=7.0)
+    @timed_state(duration=7.0)
     def launch(self, tm):
         self.catapult.launchNoSensor()
 
