ff545bdb7d36a01ad256e288a4f11aeef4c17c50
Ben1418 <ben.m.rice@gmail.com>
2014-03-19 22:18:19 -0400

    Working on Twoball Hot

--- robot/robot/src/autonomous/TwoBallHot.py
+++ robot/robot/src/autonomous/TwoBallHot.py
@@ -17,22 +17,36 @@ class TwoBall(StatefulAutonomous):
         self.register_sd_var('GetSecondBallTime',0.7);
         self.register_sd_var('DriveRotateTime2',0.1);
         self.register_sd_var('DriveRotateTime3',0.1);
+        self.decided = False
         
     def update(self, tm):
         if tm > 0.3:
             self.catapult.pulldown()
+        if not self.decided:
+            self.hotLeft = wpilib.SmartDashboard.GetBoolean("IsHotLeft")
+            self.hotRight = wpilib.SmartDashboard.GetBoolean("IsHotRight")
             
+            if (self.hotLeft or self.hotRight) and not (self.hotLeft and self.hotRight):
+                self.decided = True
+                
+                if self.hotLeft:
+                    RotateLeft()
+                    #print ("hot Left")
+                else:
+                    RotateRight()
+                    #print ('hot right')
+            else:
+                RotateLeft()
         super().update(tm)
         
     
-    @timed_state(time=1, next_state='drive_wait', first=True)
-    @timed_state(time=2,next_state='drive_start')
-    @timed_state(time=4,next_state='try_shoot')
-    @timed_state(time=5.5, next_state='next_ball1')
     
-    @timed_state(time=6.5, next_state='next_ball2')
-    @timed_state(time=7.5, next_state='launch2')
-    
+
+    def rotateRight(self):
+        self.drive_rotate_speed = self.drive_rotate_speed_right
+    def RotateLeft(self):
+        self.drive_rotate_speed = self.drive_rotate_speed_left\
+
     
     #
     #I have no idea how the timing is supposed to work from states 'nextball1' to 'launch2'.
@@ -40,27 +54,34 @@ class TwoBall(StatefulAutonomous):
     #
     #Please contact Timmy and get him to explain the timngs on his old TwoBall class
     
-    
+    @timed_state(time=7.5, next_state='launch2')    
     def launch2(self):
         
             # Finally, fire and keep firing for 1 seconds
             self.catapult.launchNoSensor()
             self.intake.ballIn()
-            
+    
+    @timed_state(time=6.5, next_state='next_ball2')        
     def next_ball2(self):
                     
             self.drive.move(0, self.drive_speed, 0)
             self.intake.ballIn()
-            
+    
+    @timed_state(time=5.5, next_state='next_ball1')        
     def next_ball1(self):
             self.drive.move(0, -1*self.drive_speed, 0)
             self.intake.ballIn()
-            
+    
+    @timed_state(time=1, next_state='drive_wait', first=True)
     def drive_wait(self, tm, state_tm):
         self.intake.armDown
+    
+    @timed_state(time=2,next_state='drive_start')
     def drive_start(self):
          self.drive.move(0, self.drive_speed, 0)
          self.intake.armDown()
+    
+    @timed_state(time=4,next_state='try_shoot')
     def try_shoot(self,tm,state_tm):
         launch(tm)
     
