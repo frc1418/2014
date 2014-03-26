a3406bb557fcaf2775828357ac7a354f482b1225
killerhamster222 <matt.puentes@gmail.com>
2014-03-26 18:43:14 -0400

    graph work

--- driver_station/src/ui/graphwindow.py
+++ driver_station/src/ui/graphwindow.py
@@ -10,7 +10,7 @@ from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanva
 #from matplotlib.backends.backend_gtkcairo import FigureCanvasGTKCairo as FigureCanvas
 
 from matplotlib.figure import Figure
-from numpy import arange, sin, pi
+from numpy import arange
 
 from widgets import (
    network_tables,
@@ -35,24 +35,29 @@ class GraphPlot(object):
         self.netTable = NetworkTable
         util.initialize_from_xml(self)
         self.Window.show_all()
+        self.netTable.PutBoolean('EnableTuning',True)
         network_tables.attach_fn(self.netTable, "Catapult Values", self.updategraph,self.Window)
         
+        
         f = Figure(figsize=(5,4), dpi=100)
-        a = f.add_subplot(111)
-        t = arange(0.0,3.0,0.01)
-        s = sin(2*pi*t)
-        a.plot(t,s)
+        self.a = f.add_subplot(111)
+        #t = arange(0.0,3.0,0.01)
+        #s = sin(2*pi*t)
+        #a.plot(t,s)
         
         canvas = FigureCanvas(f)  # a gtk.DrawingArea
 
         util.replace_widget(self.GraphImage, canvas)
         #self.Window.add(canvas)                        
         self.Window.show_all()
+        
     def updategraph(self, key, value):
-        pass
+        newestarray=network_tables.get_number_array(self.netTable, 'Catapult Values')
+        
+        
     
     def on_destroy(self, window):
-        self.netTable.putvalue('EnableTuning',False)
+        self.netTable.PutBoolean('EnableTuning',False)
         
     
 class GraphOpener(object):
@@ -60,10 +65,16 @@ class GraphOpener(object):
     def __init__(self, NetworkTable):
         self.GraphPlot = None
         self.foo=NetworkTable
+        
     def show(self):
         if self.GraphPlot==None:
             self.GraphPlot=GraphPlot(self.foo)
-            self.GraphPlot.Window.connect("on_destroy", self.destroy())
-    def destroy(self):
+            self.GraphPlot.Window.connect("destroy", self.destroy)
+            array=network_tables.get_number_array(self.foo,"Catapult Values")
+            step = 0.025
+            x = arange(0, len(array)*step, step)
+            print(array)
+            self.GraphPlot.a.plot(x, array)
+    def destroy(self, widget):
         self.GraphPlot=None;
     
