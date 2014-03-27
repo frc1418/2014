5cc0b499768c7e8acd670a9a50a71e208adf74a4
Dustin Spicuzza <dustin@virtualroadside.com>
2014-03-27 00:25:46 -0400

    Show the last three graph data received

--- driver_station/src/ui/graphwindow.py
+++ driver_station/src/ui/graphwindow.py
@@ -1,16 +1,18 @@
 
 import gtk
-import pygtk
-import util
-import glib
 import time
 import math
+
 #from matplotlib.backends.backend_gtk import FigureCanvasGTK as FigureCanvas
 from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
 #from matplotlib.backends.backend_gtkcairo import FigureCanvasGTKCairo as FigureCanvas
 
+from matplotlib.backends.backend_gtkagg import NavigationToolbar2GTKAgg as NavigationToolbar
+
 from matplotlib.figure import Figure
 from numpy import arange
+
+import util
 
 from widgets import (
    network_tables,
@@ -20,61 +22,83 @@ import logging
 logger = logging.getLogger(__name__)
 
 class GraphPlot(object):
-    # Reference Links:
-    #    Dropdown: http://www.pygtk.org/pygtk2tutorial/sec-ComboBoxAndComboboxEntry.html#comboboxbasicfig
+
     # glade file to load
     ui_filename = "GraphPlot.ui"
     
     # widgets to load from the glade file. Each one of these is added to 'self' after
     # you call 'initialize_from_xml'
     ui_widgets = [
-    'Window',
-    'GraphImage',
+        'window',
+        'graphImage',
+        'toolbar'
     ]
+    
+    HISTORY = 3
+    
     def __init__(self, NetworkTable):
-        self.netTable = NetworkTable
+        
         util.initialize_from_xml(self)
-        self.Window.show_all()
+        
+        self.dead = False
+        self.plots = []
+        
+        self.count = 0
+        
+        self.netTable = NetworkTable
         self.netTable.PutBoolean('EnableTuning',True)
-        network_tables.attach_fn(self.netTable, "Catapult Values", self.updategraph,self.Window)
         
+        self.figure = Figure(figsize=(5,4), dpi=100)
+        self.axes = self.figure.add_subplot(111)
+         
+        self.canvas = FigureCanvas(self.figure)  # a gtk.DrawingArea
         
-        f = Figure(figsize=(5,4), dpi=100)
-        self.a = f.add_subplot(111)
-        #t = arange(0.0,3.0,0.01)
-        #s = sin(2*pi*t)
-        #a.plot(t,s)
+        self.graphImage = util.replace_widget(self.graphImage, self.canvas)
+        self.toolbar = util.replace_widget(self.toolbar, NavigationToolbar(self.canvas, self.window))
+                           
+        self.window.show_all()
         
-        canvas = FigureCanvas(f)  # a gtk.DrawingArea
-
-        util.replace_widget(self.GraphImage, canvas)
-        #self.Window.add(canvas)                        
-        self.Window.show_all()
+        # listen to network tables variables
+        network_tables.attach_fn(self.netTable, "Catapult Values", self.on_update_CatapultValues, self.window)
+        network_tables.attach_fn(self.netTable, "EnableTuning", self.on_update_EnableTuning, self.window)
         
-    def updategraph(self, key, value):
-        newestarray=network_tables.get_number_array(self.netTable, 'Catapult Values')
+    def on_update_EnableTuning(self, key, value):
+        if not self.dead and not value:
+            self.netTable.PutBoolean('EnableTuning', True)
         
+    def on_update_CatapultValues(self, key, value):
         
+        array = network_tables.get_number_array(self.netTable, key)
+        self.count += 1
+        
+        step = 0.025
+        x = arange(0, len(array)*step, step)
+        
+        plot = self.axes.plot(x, array, label=str(self.count))
+        
+        # clear old things
+        if len(self.axes.lines) > self.HISTORY:
+            self.axes.lines.pop(0)
+            
+        self.axes.legend()
+        self.canvas.draw() 
     
     def on_destroy(self, window):
+        self.dead = True
         self.netTable.PutBoolean('EnableTuning',False)
         
     
 class GraphOpener(object):
     
     def __init__(self, NetworkTable):
-        self.GraphPlot = None
-        self.foo=NetworkTable
+        self.graphPlot = None
+        self.netTable = NetworkTable
         
     def show(self):
-        if self.GraphPlot==None:
-            self.GraphPlot=GraphPlot(self.foo)
-            self.GraphPlot.Window.connect("destroy", self.destroy)
-            array=network_tables.get_number_array(self.foo,"Catapult Values")
-            step = 0.025
-            x = arange(0, len(array)*step, step)
-            print(array)
-            self.GraphPlot.a.plot(x, array)
-    def destroy(self, widget):
-        self.GraphPlot=None;
+        if self.graphPlot == None:
+            self.graphPlot = GraphPlot(self.netTable)
+            self.graphPlot.window.connect("destroy", self.on_destroy)
+     
+    def on_destroy(self, widget):
+        self.graphPlot=None
     
