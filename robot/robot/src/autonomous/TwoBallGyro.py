1649ae7f9367db06a082124761b7b3cf0aae53ac
Dustin Spicuzza <dustin@virtualroadside.com>
2014-03-27 20:38:17 -0400

    Separate two ball and two ball gyro

--- robot/robot/src/autonomous/TwoBallGyro.py
+++ robot/robot/src/autonomous/TwoBallGyro.py
@@ -7,14 +7,16 @@ from common.autonomous_helper import StatefulAutonomous, timed_state
 
 class TwoBall(StatefulAutonomous):
     
-    MODE_NAME = 'Two balls'
-    DEFAULT = True
+    MODE_NAME = 'Two ball gyro'
+    DEFAULT = False
      
     def __init__(self, components):
         super().__init__(components)
         
         self.register_sd_var('drive_speed', 0.5)
+        
     def on_enable(self):
+        self.drive.reset_gyro_angle()
         super().on_enable()
         
     def update(self, tm):
@@ -29,17 +31,20 @@ class TwoBall(StatefulAutonomous):
     def drive_wait(self, tm, state_tm):
         '''Wait some period before we start driving'''
         self.intake.armDown()
+        self.drive.angle_rotation(0)
         
     @timed_state(duration=1.4, next_state='launch')
     def drive(self, tm, state_tm):
         '''Start the launch sequence! Drive slowly forward for N seconds'''
         self.drive.move(0, self.drive_speed, 0)
         self.intake.armDown()
+        self.drive.angle_rotation(0)
     
     @timed_state(duration=1, next_state='go_back') 
     def launch(self, tm):
         '''Fire and keep firing for 1 seconds'''
         self.catapult.launchNoSensor()
+        self.drive.angle_rotation(0)
         
     
     @timed_state(duration=2.9,next_state='drive2') 
@@ -47,15 +52,18 @@ class TwoBall(StatefulAutonomous):
         '''Go back to get the next ball'''
         self.drive.move(0, -1*self.drive_speed, 0)
         self.intake.ballIn()
+        self.drive.angle_rotation(0)
             
     @timed_state(duration=2.5, next_state='launch2', first=False)
     def drive2(self, tm, state_tm):
         '''Once we get it, drive forward'''
         self.drive.move(0, self.drive_speed, 0)
         self.intake.ballIn()
+        self.drive.angle_rotation(0)
     
     @timed_state(duration=1) 
     def launch2(self, tm):
         '''And shoot!'''
         self.catapult.launchNoSensor()
         self.intake.ballIn()
+        self.drive.angle_rotation(0)
